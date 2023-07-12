4f7a5c9742018d9f16865511e967b74038a0743c
Jakub Pawlik <jakub.pawlik@vp.pl>
2023-07-12 07:51:49 +0200

    I guess I forgot to send the last changes

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,7 +1,5 @@
 import PropTypes from 'prop-types';
 import css from './Section.module.css';
-// import { Statistics } from '../Statistics/Statistics';
-// import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
 
 export const Section = ({ title, children }) => {
   return (
